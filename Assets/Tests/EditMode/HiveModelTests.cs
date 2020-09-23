015c3475700b6ff146c2e4e9267ff77d0f355919
Daniel Gili López <dgl@uoc.edu>
2020-09-23 18:09:28 +0200

    Es creen els directoris Resources i Tests

---
+++ Assets/Tests/EditMode/HiveModelTests.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class HiveModelTests
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void HiveModelSimplePasses()
+        {
+            Board board = new Board();
+            board.Initialize();
+            board.MovePiece(board.GetNotPlacedPieces()[0], (0, 0, 0));
+
+            Assert.IsTrue(board.GetPlacedPieces().ContainsKey((0, 0, 0)));
+            // TODO: CUANDO PUEDA CARGAR Y GUARDAR JUGADAS DESDE JSON, TENDRÉ QUE DESARROLLAR LOS TEST
+        }
+
+    }
+}
