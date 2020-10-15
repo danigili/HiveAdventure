a0c4ca5cb17b31cc088a6e1e2372c77412447354
Daniel Gili López <dgl@uoc.edu>
2020-10-15 23:01:59 +0200

    Polish pieces panel script

--- Assets/Scripts/UI/PiecesPanel.cs
+++ Assets/Scripts/UI/PiecesPanel.cs
@@ -15,64 +15,102 @@ public class PiecesPanel : MonoBehaviour
     private float lastPos = 0;
     private Action<PieceUI> clickPieceCallback;
     public List<PieceUI> pieces;
+    public List<GameObject> objects;
+
+    public void Hola()
+    {
+        foreach (PieceUI pui in pieces)
+            if (pui.gameObject.activeSelf)
+                PlacePiece(pui.gameObject);
+    }
+
     public void Initialize(Board model, Action<PieceUI> callback)
     {
         this.model = model;
         clickPieceCallback = callback;
         PlacePieces();
+        PanelPosition();
     }
 
     private void PlacePieces()
     {
+        pieces.Clear();
+        // Disable current pieces
+        foreach (GameObject obj in objects)
+            obj.SetActive(false);
+
+        Transform child = transform.GetChild(0);
+        int j = 0;
         foreach (Piece piece in model.GetNotPlacedPieces())
         {
             if (side != piece.side) continue;
-            Transform child = transform.GetChild(0);
-            bool placed = false;
+
             GameObject instance;
-            
-            Vector2 pos;
-            // Put the piece next to a piece of the same type
-            for (int i = child.childCount-1; i >= 0 && !placed; i--)
+            if (objects.Count > j && !objects[j].activeSelf)
             {
-                if (child.GetChild(i).GetComponent<PieceUI>().piece.type == piece.type)
-                {
-                    instance = Instantiate((GameObject)Resources.Load("Prefabs/PieceUI"), child);
-                    pos.x = child.GetChild(i).GetComponent<RectTransform>().anchoredPosition.x + xMargin;
-                    pos.y = child.GetChild(i).GetComponent<RectTransform>().anchoredPosition.y;
-                    instance.GetComponent<PieceUI>().Initialize(piece, pos, ClickPiece);
-                    pieces.Add(instance.GetComponent<PieceUI>());
-                    child.GetChild(i).GetComponent<Button>().enabled = false;
-                    placed = true;
-                }
-            }
-            if (placed) continue;
-            
-            // If not, place under the last piece
-            if (child.childCount > 0)
-            {
-                instance = Instantiate((GameObject)Resources.Load("Prefabs/PieceUI"), child);
-                pos.x = StartPosition.anchoredPosition.x;
-                pos.y = lastPos - yMargin;
-                lastPos = pos.y;
-                instance.GetComponent<PieceUI>().Initialize(piece, pos, ClickPiece);
-                instance.GetComponent<RectTransform>().anchoredPosition = pos;
+                instance = objects[j];
             }
             else
-            // If there aren`t pieces, place in the start position
             {
                 instance = Instantiate((GameObject)Resources.Load("Prefabs/PieceUI"), child);
-                pos.x = StartPosition.anchoredPosition.x;
-                pos.y = StartPosition.anchoredPosition.y;
-                lastPos = pos.y;
-                instance.GetComponent<PieceUI>().Initialize(piece, pos, ClickPiece);
-                instance.GetComponent<RectTransform>().anchoredPosition = pos;
+                instance.gameObject.SetActive(false);
+                objects.Add(instance);
             }
+            j++;
+            instance.GetComponent<PieceUI>().Initialize(piece, ClickPiece);
+
+            PlacePiece(instance);
             pieces.Add(instance.GetComponent<PieceUI>());
         }
-        Vector2 panelPos = transform.GetComponent<RectTransform>().anchoredPosition;
-        panelPos.y = Hieght();
-        transform.GetComponent<RectTransform>().anchoredPosition = panelPos;
+        
+    }
+
+    private void PlacePiece(GameObject instance)
+    {
+        Transform child = transform.GetChild(0);
+        bool placed = false;
+        bool firstPiece = true;
+        Vector2 pos;
+        if (instance.GetComponent<PieceUI>().piece.type == BugType.Ant)
+            Debug.Log("");
+        // Put the piece next to a piece of the same type
+        for (int i = child.childCount - 1; i >= 0 && !placed; i--)
+        {
+            if (child.GetChild(i).gameObject.activeSelf)
+            {
+                firstPiece = false;
+                if (child.GetChild(i).GetComponent<PieceUI>().piece.type == instance.GetComponent<PieceUI>().piece.type)
+                {
+                    pos.x = child.GetChild(i).GetComponent<RectTransform>().anchoredPosition.x + xMargin;
+                    pos.y = child.GetChild(i).GetComponent<RectTransform>().anchoredPosition.y;
+                    instance.GetComponent<RectTransform>().anchoredPosition = pos;
+                    instance.GetComponent<PieceUI>().SetPosition(pos);
+                    child.GetChild(i).GetComponent<Button>().enabled = false;
+                    placed = true;
+                    break;
+                }
+            }
+        }
+        if (placed) return;
+
+        // If not, place under the last piece
+        if (!firstPiece)
+        {
+            pos.x = StartPosition.anchoredPosition.x;
+            pos.y = lastPos - yMargin;
+            lastPos = pos.y;
+            instance.GetComponent<PieceUI>().SetPosition(pos);
+            instance.GetComponent<RectTransform>().anchoredPosition = pos;
+        }
+        else
+        // If there aren`t pieces, place in the start position
+        {
+            pos.x = StartPosition.anchoredPosition.x;
+            pos.y = StartPosition.anchoredPosition.y;
+            lastPos = pos.y;
+            instance.GetComponent<PieceUI>().SetPosition(pos);
+            instance.GetComponent<RectTransform>().anchoredPosition = pos;
+        }
     }
 
     private float Hieght()
@@ -82,6 +120,13 @@ public class PiecesPanel : MonoBehaviour
             return -hieght;
         else
             return hieght;
+    }
+
+    private void PanelPosition()
+    {
+        Vector2 panelPos = transform.GetComponent<RectTransform>().anchoredPosition;
+        panelPos.y = Hieght();
+        transform.GetComponent<RectTransform>().anchoredPosition = panelPos;
     }
 
     public void ClickPiece(PieceUI piece)
