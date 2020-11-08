99bb9b3fea4a8fadef3f598a90120d14c569fa62
Daniel Gili López <dgl@uoc.edu>
2020-11-08 12:18:46 +0100

    Add translations

---
+++ Assets/Scripts/UI/TextUI.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextUI : MonoBehaviour
+{
+    public string key;
+
+    private Language language;
+    private Text textComponent;
+    private TextMesh textMesh;
+
+    void Start()
+    {
+        gameObject.TryGetComponent<Text>(out textComponent);
+        gameObject.TryGetComponent<TextMesh>(out textMesh);
+        SetText();
+    }
+
+    void Update()
+    {
+        if (language != Localization.GetCurrentLanguage())
+            SetText();
+    }
+
+    private void SetText()
+    {
+        language = Localization.GetCurrentLanguage();
+        if (textComponent != null)
+            textComponent.text = Localization.Translate(key);
+        if (textMesh != null)
+            textMesh.text = Localization.Translate(key);
+    }
+}
